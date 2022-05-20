faf19df4cbd98ddd702f86c575a97fdec0c85b0b
радон <96480522+RadonRebirth@users.noreply.github.com>
2022-05-20 10:44:58 +0300

    Update Main.java

--- laba13/src/com/company/Main.java
+++ laba13/src/com/company/Main.java
@@ -3,22 +3,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try{
-            FileReader file = new FileReader("code.txt");
-            int count = 0;
-            String text = "";
-            while((count = file.read()) != -1){
-                text += (char)count;
-            }
-            String a = text.replaceAll("\\//.+", "");
-            String c = a.replaceAll("\\/\\*([\\S\\s]+)\\*\\/", "");
+        while(true) {
+            try(FileReader file = new FileReader("code.txt"); FileWriter fileWriter = new FileWriter("result.txt")) {
+                int count = 0;
+                String text = "";
+                while ((count = file.read()) != -1) {
+                    text += (char) count;
+                }
+                String a = text.replaceAll("\\//.+", "");
+                String c = a.replaceAll("\\/\\*([\\S\\s]+)\\*\\/", "");
 
-            FileWriter fileWriter = new FileWriter("result.txt");
-            fileWriter.write(c);
-            fileWriter.flush();
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
+                fileWriter.write(c);
+                fileWriter.flush();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 }
